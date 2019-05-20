c3f527dbc15fa81b5181a1a1e1fcb20b894cdb88
Juan Manuel Rodriguez Defago <juanma.rdefago@gmail.com>
2019-05-20 01:39:21 -0300

    Re-initializing repo

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'Home',
+  data () {
+    return {
+      accommodations: []
+    }
+  },
+  mounted () {
+    axios
+      .get()
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
