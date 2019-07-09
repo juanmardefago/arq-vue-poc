322db892d7c9548dd67698af783b7d0604bc074f
Juan Manuel Rodriguez Defago <juanma.rdefago@gmail.com>
2019-07-09 01:27:22 -0300

    possible fix for mongo transient network error

--- backend/src/mongoose.js
+++ backend/src/mongoose.js
@@ -1,10 +1,25 @@
 const mongoose = require('mongoose');
 
+function connectToMongo(uri, options, retriesLeft, retryDelay) {
+  mongoose.connect(uri, options)
+    .then(() => console.log("MongoDB client connected"))
+    .catch((err) => {
+      console.log("MongoDB connection failed, error: ");
+      console.log(err);
+      if(retriesLeft > 0) {
+        setTimeout(connectToMongo(uri, options, retriesLeft - 1), retryDelay);
+      }
+    });
+}
+
 module.exports = function (app) {
-  mongoose.connect(
-    app.get('mongodb'),
-    { useCreateIndex: true, useNewUrlParser: true }
-  );
+  var uri = app.get('mongodb');
+  var options = { useCreateIndex: true, useNewUrlParser: true };
+  var retries = app.get('mongoConnectionRetries');
+  var retryDelay = app.get('mongoConnectionRetryDelay');
+
+  connectToMongo(uri, options, retries, retryDelay);
+
   mongoose.Promise = global.Promise;
 
   app.set('mongooseClient', mongoose);
