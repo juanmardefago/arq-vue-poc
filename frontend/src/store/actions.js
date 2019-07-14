3e89992e8ae01bdb66eb5f43ea4d9483b34a4303
Juan Manuel Rodriguez Defago <juanma.rdefago@gmail.com>
2019-07-14 14:23:03 -0300

    Refactor signin/singup

--- frontend/src/store/actions.js
+++ frontend/src/store/actions.js
@@ -1,1 +1,13 @@
-export default {};
+import axios from "axios";
+
+export default {
+  createUser(context, user) {
+    return axios.post(`${process.env.VUE_APP_BACKEND_URL}/users`, user);
+  },
+  authenticate(context, user) {
+    return axios.post(
+      `${process.env.VUE_APP_BACKEND_URL}/authentication`,
+      user
+    );
+  }
+};
