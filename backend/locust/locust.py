326bd8a859fffe5c13384b1a2de9cd5bcad67ebe
Juan Manuel Rodriguez Defago <juanma.rdefago@gmail.com>
2019-11-26 18:10:19 -0300

    Added more locust cases

--- backend/locust/locust.py
+++ backend/locust/locust.py
@@ -1,32 +1,94 @@
-from locust import HttpLocust, TaskSet, task, between
+from locust import HttpLocust, TaskSet, TaskSequence, task, between
+import random
 
-class LoginListBehavior(TaskSet):
+usernames = ["pepe", "pepa", "roberto", "alberto", "laura", "loro"]
+domains = ["@pepe.com", "@loro.com", "@camba.coop", "@medium.org", "@hackerspace.com"]
+provinces = ["Buenos Aires", "Catamarca", "Entre Rios"]
+
+
+class AdminTaskSet(TaskSet):
 
     auth_token = ""
 
     def on_start(self):
+        self.create_user()
         self.login()
-
-    @task(1)
-    def accomodations(self):
-        self.accommodations()
 
     def login(self):
         response = self.client.post("/authentication", {
-          "strategy": "local",
-          "email": "admin",
-          "password": "admin"
+        "strategy": "local",
+        "email": "admin@arq1-app.com",
+        "password": "admin"
         }).json()
 
         self.auth_token = response["accessToken"]
 
-    def accommodations(self):
-        response = self.client.get("/accommodation", 
-        headers= { "Authorization": self.auth_token }).json()
-        print(response)
-     
+    def get_approved_accommodations_list(self):
+        response = self.client.get("/accommodation?approved=true&$limit=10",
+        headers= { "Authorization": self.auth_token })
+        return response
 
-class WebsiteUser(HttpLocust):
-    task_set = LoginListBehavior
-    wait_time = between(5, 9)
+    def get_unapproved_accommodations_list(self):
+        response = self.client.get("/accommodation?approved=false&$limit=10",
+        headers= { "Authorization": self.auth_token })
+        return response
 
+    def create_accommodation(self):
+        response = self.client.post("/accommodation",
+        json={
+          "location": {
+            "province": {
+              "name": random.choice(provinces),
+              "id": 1
+            },
+            "city": {
+              "name": random.choice(provinces),
+              "id": 1
+            },
+            "address": "asdasd address"
+          },
+          "category": random.randrange(1, 5, 1),
+          "type": "Hotel",
+          "pensions": {
+            "breakfast": 10,
+            "halfPension": 20,
+            "fullPension": 30
+          }
+        },
+        headers= { "Authorization": self.auth_token })
+        return response
+
+    def create_user(self):
+        response = self.client.post("/users",
+        json={
+          "email": random.choice(usernames) + str(random.randrange(0, 999999999999, 1)) + random.choice(domains),
+          "password": "sarasa",
+          "permissions": "user"
+        },
+        headers= { "Authorization": self.auth_token })
+        return response
+
+
+    @task(30)
+    def list_approved_accommodations_task(self):
+        self.get_approved_accommodations_list()
+
+    @task(30)
+    def list_unapproved_accommodations_task(self):
+        self.get_unapproved_accommodations_list()
+
+    @task(5)
+    def create_random_accommodation(self):
+        self.create_accommodation()
+
+    @task(100)
+    def edit_random_accommodation(self):
+        response = self.get_approved_accommodations_list()
+
+        if response.status_code == 200:
+            json = response.json();
+            json.data[random.randrange(0, json.total, 1)]._id
+
+class WebsiteAdmin(HttpLocust):
+    task_set = AdminTaskSet
+    wait_time = between(2, 5)
